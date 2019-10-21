72c86ec04c5fe2e72797509bb8a5b8f0a8bc7df6
JTimerman <joaquin.timerman@gmail.com>
2019-10-21 16:24:00 -0300

    Rearange App imports

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import "./App.css";
-import Home from "./Home";
-import Test from "./Test";
 import PrivateRoute from "./components/PrivateRoute";
 import SignIn from "./components/SignIn";
+
+import Home from "./Home";
+import Test from "./Test";
 import Logout from "./Logout";
 import store from "./redux/store";
+import "./App.css";
+
 window.store = store;
 
 function App() {
