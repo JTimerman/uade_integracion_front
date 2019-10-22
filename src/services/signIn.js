fe41df8fb7c0f363422234dabe5277ec8bee191a
JTimerman <joaquin.timerman@gmail.com>
2019-10-22 09:38:05 -0300

    Add services for holders

--- src/services/signIn.js
+++ src/services/signIn.js
@@ -4,11 +4,5 @@ import { BASE_URL, SIGN_IN } from "../constants/endpoints.json";
 export default (username, password) => {
   const body = { username, password };
 
-  return fetchAPI(BASE_URL + SIGN_IN, "POST", body).then(response => {
-    if (response.status === 401) {
-      return Promise.reject();
-    } else {
-      return response.json();
-    }
-  });
+  return fetchAPI(BASE_URL + SIGN_IN, "POST", body);
 };
