a8113be0a1eae6bac2856ec6d9b35e8343363541
JTimerman <joaquin.timerman@gmail.com>
2019-10-24 11:49:57 -0300

    Refacto SIgnIn and Simple Dialog

--- src/components/SignIn/signIn.js
+++ src/components/SignIn/signIn.js
@@ -1,24 +1,18 @@
 import React, { useState } from "react";
+import { withRouter } from "react-router-dom";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
-
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Grid from "@material-ui/core/Grid";
-
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 
-import Container from "@material-ui/core/Container";
-import useStyles from "./styles";
-import { withStyles } from "@material-ui/core/styles";
-import { withRouter } from "react-router-dom";
-import { CircularProgress } from "@material-ui/core";
-
-const SignIn = props => {
-  const classes = useStyles();
+const SignIn = ({ authenticateUser, history, classes }) => {
   const [values, setValues] = React.useState({});
   const [error, setError] = React.useState(false);
   const [loading, setLoading] = useState(false);
@@ -32,10 +26,9 @@ const SignIn = props => {
     event.preventDefault();
     const { username, password } = values;
 
-    props
-      .authenticateUser(username, password)
+    authenticateUser(username, password)
       .then(() => {
-        props.history.push("/");
+        history.push("/");
         setLoading(false);
       })
       .catch(() => {
@@ -116,6 +109,5 @@ const SignIn = props => {
     </Container>
   );
 };
-const styledComponent = withStyles(useStyles)(SignIn);
 
-export default withRouter(styledComponent);
+export default withRouter(SignIn);
--- src/components/SimpleDialog/index.js
+++ src/components/SimpleDialog/index.js
@@ -1,11 +1,33 @@
 import { connect } from "react-redux";
+import { withStyles } from "@material-ui/core/styles";
+import { blue } from "@material-ui/core/colors";
 
 import { getFilteredEmployees } from "../../redux/selectors/employees";
 
 import SimpleDialog from "./SimpleDialog";
 
+const styles = theme => ({
+  avatar: {
+    backgroundColor: blue[100],
+    color: blue[600]
+  },
+  select: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    marginTop: "5%"
+  },
+  dialogContent: {
+    width: "700px"
+  },
+  items: {
+    marginLeft: "-4%"
+  }
+});
+
+const styledComponent = withStyles(styles)(SimpleDialog);
+
 const mapStateToProps = store => ({
   employees: getFilteredEmployees(store)
 });
 
-export default connect(mapStateToProps)(SimpleDialog);
+export default connect(mapStateToProps)(styledComponent);
