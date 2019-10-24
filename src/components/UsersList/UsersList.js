3b0796bc1d428a61a809fa936bdcb63e3e05f980
denbalestri <denisbalestri@hotmail.com>
2019-10-24 11:11:46 -0300

    feature/addListUser delete imports

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,33 +1,12 @@
 import React from "react";
 import { withStyles, makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import useStyles from "./styles";
-import TablePagination from "@material-ui/core/TablePagination";
+
 import MaterialTable from "material-table";
-const columns = [
-  { id: "firstName", label: "First name", minWidth: 170 },
-  { id: "lastName", label: "Last name", minWidth: 100 },
-  { id: "phone", label: "Phone number", minWidth: 100 },
-  { id: "address", label: "Address", minWidth: 100 },
-  { id: "rol", label: "Rol", minWidth: 100 },
-  { id: "email", label: "E-mail", minWidth: 100 }
-];
-
-function createData(firstName, lastName, phone, address, rol, email) {
-  return { firstName, lastName, phone, address, rol, email };
-}
-
-const rows = [createData(), createData()];
 
 const UsersList = () => {
   const classes = useStyles();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
   const [state, setState] = React.useState({
     columns: [
@@ -66,15 +45,6 @@ const UsersList = () => {
       }
     ]
   });
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = event => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
 
   return (
     <Paper className={classes.root}>
